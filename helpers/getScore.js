bad69f84646983d77f07c54d4fbd17c2db913f0a
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-26 17:33:06 -0700

    a score is now showing up on each suggestion

---
+++ helpers/getScore.js
@@ -0,0 +1,7 @@
+// will implement a more precise approach later
+
+function getScore(name, latitude, longitude) {
+  return Math.random();
+}
+
+module.exports = getScore;
--- routes/suggestions.js
+++ routes/suggestions.js
@@ -2,12 +2,14 @@
 
 const express = require('express');
 const Suggestion = require('../models/suggestions');
+const getScore = require('../helpers/getScore');
 
 const router = new express.Router();
 
 router.get('/', async function(req, res, next) {
-  const query = req.query.q;
-  const suggestions = await Suggestion.findAll({name: query});
+  let query = req.query.q;
+  let suggestions = await Suggestion.findAll({name: query});
+  suggestions = suggestions.map(suggestion => ({score: getScore(), ...suggestion}));
   return res.json({ suggestions });
 })
 
